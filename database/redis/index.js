9026d9632057d181a8c3e41104508f7b1bf6fbf6
gaborszekely <gabor_szekely@hotmail.com>
2019-05-03 15:53:13 -0600

    Finished SDC

--- database/redis/index.js
+++ database/redis/index.js
@@ -1,7 +1,6 @@
 const redis = require('redis');
 
 const defaultConnection = 'redis://gabe-sdc-redis.guujxr.0001.use2.cache.amazonaws.com:6379';
-
-const client = redis.createClient(/* process.env.REDIS_URL || defaultConnection */);
+const client = redis.createClient(process.env.REDIS_URL || defaultConnection);
 
 module.exports = client;
